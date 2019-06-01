6454ceb60ffadd2dcb4e2aadb152b88363b02047
chifuresearch <chifu.research@gmail.com>
2019-06-01 19:15:07 +0800

    Updates

---
+++ precache-manifest.9b285b084ad8042311175e942331893a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99c164eb09dedcb20dbfdae6245a3b4",
+    "url": "/chrlabsite/index.html"
+  },
+  {
+    "revision": "876712227087a7cd0e27",
+    "url": "/chrlabsite/static/css/2.73b94da4.chunk.css"
+  },
+  {
+    "revision": "d153cddd3a7147c04393",
+    "url": "/chrlabsite/static/css/main.e9b5a44f.chunk.css"
+  },
+  {
+    "revision": "876712227087a7cd0e27",
+    "url": "/chrlabsite/static/js/2.ba05769b.chunk.js"
+  },
+  {
+    "revision": "d153cddd3a7147c04393",
+    "url": "/chrlabsite/static/js/main.e669395b.chunk.js"
+  },
+  {
+    "revision": "e73134cdb39373da26a0",
+    "url": "/chrlabsite/static/js/runtime~main.6f4bc901.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/chrlabsite/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/chrlabsite/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/chrlabsite/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/chrlabsite/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/chrlabsite/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/chrlabsite/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/chrlabsite/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/chrlabsite/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/chrlabsite/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/chrlabsite/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/chrlabsite/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chrlabsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/chrlabsite/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/chrlabsite/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/chrlabsite/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/chrlabsite/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/chrlabsite/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/chrlabsite/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/chrlabsite/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/chrlabsite/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/chrlabsite/static/media/slick.f97e3bbf.svg"
+  }
+]);
