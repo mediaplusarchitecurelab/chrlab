1c2b73d2d7107038747ceb7d0c954417217556e5
chifuresearch <chifu.research@gmail.com>
2019-09-11 00:56:56 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chrlab/precache-manifest.a706ab3163f454ace1e21f0e8c6a3618.js"
+  "/chrlab/precache-manifest.a315895e1a5c13e1c0f35633ba57c567.js"
 );
 
 self.addEventListener('message', (event) => {
